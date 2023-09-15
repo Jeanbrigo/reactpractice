580658fdcb8984552e2131c89ec4e6291fb28c06
Jean Brigonnet <jeanbrigo@gmail.com>
2023-09-14 22:52:02 -0500

    working login/logout routes

---
+++ src/pages/login.js
@@ -0,0 +1,9 @@
+import {Form} from "react-router-dom"
+
+export default function Login(){
+    return <Form action="/login" method="post">
+        <input type="text" name="username" placeholder="username"/>
+        <input type="text" name="password" placeholder="password"/>
+        <button>Log In</button>
+    </Form>
+}
